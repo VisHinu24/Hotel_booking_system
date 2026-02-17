e1db229bd33da32e95c8477c1c8c6604acdd503b
Bhuvana23013531 <manogarbhuvana@gmail.com>
2026-02-17 17:15:57 +0530

    add security

--- hotel_booking/src/main/java/com/example/hotel_booking/Security/AuthController.java
+++ hotel_booking/src/main/java/com/example/hotel_booking/Security/AuthController.java
@@ -1,7 +1,10 @@
 package com.example.hotel_booking.Security;
 
+import com.example.hotel_booking.model.User;
+import com.example.hotel_booking.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.*;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -13,6 +16,30 @@ public class AuthController {
 
     @Autowired
     private JwtUtil jwtUtil;
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @PostMapping("/register")
+    public String register(@RequestBody RegisterRequest request) {
+
+        // Check if username already exists
+        if (userRepository.findByUsername(request.getUsername()).isPresent()) {
+            return "Username already exists!";
+        }
+
+        User user = new User();
+        user.setUsername(request.getUsername());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole(Role.USER);
+
+        userRepository.save(user);
+
+        return "User registered successfully";
+    }
 
     @PostMapping("/login")
     public AuthResponse login(@RequestBody AuthRequest request) {
